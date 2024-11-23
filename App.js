7a22fc8870aa91676a79c3ab0c6f75aa25510750
Shreedhar <shreedharsree799@gmail.com>
2024-11-23 17:00:09 +0530

    Initial commit: First version of the project

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={tw.style("text-red-600 font-bold text-xl")}>Open up App.js to start working on shreedhar!</Text>
+      <Text style={tw.style("text-xl")}>
+        Version 1
+      </Text>
       <StatusBar style="auto" />
     </View>
   );

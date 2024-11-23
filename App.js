1e1724052928ae5e2781269f77d5d4a65ded7107
Shreedhar <shreedharsree799@gmail.com>
2024-11-23 17:07:53 +0530

    Version 2: Added new features and improvements

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ import tw from 'tailwind-react-native-classnames';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={tw.style("text-red-600 font-bold text-xl")}>Open up App.js to start working on shreedhar!</Text>
+      <Text style={tw.style("text-red-600 font-bold text-xl")}>Successfully version 2</Text>
       <Text style={tw.style("text-xl")}>
-        Version 1
+        Version 2
       </Text>
       <StatusBar style="auto" />
     </View>
